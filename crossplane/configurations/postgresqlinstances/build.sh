150a59b0b987465797695571e102ef123e13f9ee
driosalido <driosalido@gmail.com>
2022-05-23 17:39:12 +0200

    moaaarr changes

--- crossplane/configurations/postgresqlinstances/build.sh
+++ crossplane/configurations/postgresqlinstances/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+VERSION="v1.0.2"
 rm *.xpkg
 docker login
 kubectl crossplane build configuration
-kubectl crossplane push configuration driosalido/aws-rds-postresql:v1.0.1
+kubectl crossplane push configuration driosalido/aws-rds-postresql:${VERSION}
 
+#kubectl crossplane install configuration driosalido/aws-rds-postresql:${VERSION}
